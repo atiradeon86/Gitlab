9b60a4ffa08cf0600dfd07c0c00371fdedacead2
atiradeon86 <atiradeon86@gmail.com>
2022-11-20 18:31:15 +0100

    Web deployment

---
+++ web-deployment/index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <meta name="robots" content="noindex">
+  <title>Bryan PHPInfo Deployment Test ... :)</title>
+ </head>
+ <body>
+    <h1>Always Look on the Bright Side of Life ...</h>
+    <?php phpinfo(); ?> 
+</body>
+</html>
